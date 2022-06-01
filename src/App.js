7efbd2252f93ab5032b44309cfc9266ca74c48a8
unknown <cmejia106@gmail.com>
2022-06-01 14:58:52 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import ReactPlayer from 'react-player';
+
+import {ALAMEDA, SAN_ANTONIO} from './videos/videos'
+
 
 function App() {
+  
+  const listaReproduccion = (sede) => {
+    const list = []
+    Object.keys(sede).forEach(key => list.push(sede[key]))
+    return list;
+  };
+
+  const Buttons = () =>{
+    return (
+      <div class="btn-group" role="group" aria-label="Basic example">
+        
+        < Link to='/alameda'><button type="button" className="btn btn-secondary">Alameda</button></Link>
+        < Link to='/san-antonio'><button type="button" className="btn btn-secondary">San Antonio</button></Link>
+
+      </div>
+     
+
+    );
+  }
+
+
   return (
+    
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+
+          <Route path='/' element={< Buttons />} />
+      
+          <Route path="alameda" element={< ReactPlayer url= {listaReproduccion(ALAMEDA)} loop controls playing width='100%' height='100%' className='react-player'/>} />
+
+          <Route path="san-antonio" element={< ReactPlayer url= {listaReproduccion(SAN_ANTONIO)} loop controls playing width='100%' height='100%' className='react-player'/>} />
+
+        </Routes>
+      </BrowserRouter>
+      
     </div>
+
   );
 }
 
---
+++ src/videos/videos.js
@@ -0,0 +1,16 @@
+
+export const ALAMEDA = {
+
+    tortas:'https://youtu.be/rIoJXtbl9Es',
+    malteadas:'https://youtu.be/04UmtEeDbVw',
+    desayunos:'https://youtu.be/B55KkrWWGs0',
+
+};
+
+export const SAN_ANTONIO = {
+
+    /* tortas:'', */
+    malteadas:'https://youtu.be/0Qr9esjf1Gw',
+    desayunos:'https://youtu.be/vol1T72N_3k',
+
+};
