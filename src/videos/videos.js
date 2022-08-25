e6a907b87d2af1e7ad91fc7b5f3ae722eccdbb62
unknown <cmejia106@gmail.com>
2022-08-25 07:11:57 -0500

    talleres

--- src/videos/videos.js
+++ src/videos/videos.js
@@ -1,87 +1,89 @@
 
-export const ALAMEDA = {
 
-    tortas:'https://youtu.be/rIoJXtbl9Es',
-    malteadas:'https://youtu.be/04UmtEeDbVw',
-    desayunos:'https://youtu.be/VdlVi6IASOg',
-    destacado1: 'https://youtu.be/PhpmSb58zIc',
-    destacado2: 'https://youtu.be/1HJNM8RCQSM',
-    postre:'https://youtu.be/pNSWtvuawQI',
-    cocteles:'https://youtu.be/R-KPSDuQ6SE',
-    sedes: 'https://youtu.be/HE4T2fGujYo',
-    te_chai: 'https://youtu.be/eEQbnoevhF0'
+    /* :::::::::::::  ALAMEDA :::::::::::::::: */
+    const a_tortas='https://youtu.be/rIoJXtbl9Es';
+    const a_malteadas='https://youtu.be/04UmtEeDbVw';
+    const a_desayunos='https://youtu.be/a2cHWbZsvWY';
+    const a_destacado1='https://youtu.be/PhpmSb58zIc';
+    const a_destacado2='https://youtu.be/1HJNM8RCQSM';
+    const a_postre='https://youtu.be/pNSWtvuawQI';
+    const a_cocteles='https://youtu.be/R-KPSDuQ6SE';
 
-};
+    /* :::::::::::::: SAN ANTONIO :::::::::::: */
+    const malteadas='https://youtu.be/0Qr9esjf1Gw';
+    const desayunos='https://youtu.be/vol1T72N_3k';
+    const cocteles='https://youtu.be/R-KPSDuQ6SE';
+    const focaccia_sandwiches='https://youtu.be/JFdiQ4xVp7Y';
+    const cafe_licor='https://youtu.be/T5NYEPxDXC4';
+    const postres='https://youtu.be/fFNotkvMB5Q';
+    
+
+    /* ::::::::::::::: GENERALES ::::::::: */
+    const sedes='https://youtu.be/HE4T2fGujYo';
+    const taller='https://youtu.be/CGGaxlx_eZI';
+    const te_chai='https://youtu.be/eEQbnoevhF0';
+
 
 export const FDS_MORNING_ALAMEDA={
-    desayunos:'https://youtu.be/VdlVi6IASOg',
-    destacado1: 'https://youtu.be/PhpmSb58zIc',
-    destacado2: 'https://youtu.be/1HJNM8RCQSM',
-    sedes: 'https://youtu.be/HE4T2fGujYo',
-    te_chai: 'https://youtu.be/eEQbnoevhF0' 
-        
+    a_desayunos,
+    a_destacado1,
+    a_destacado2,
+    sedes,
+    te_chai         
 };
 
 export const FDS_TARDE_ALAMEDA={
-    cocteles:'https://youtu.be/R-KPSDuQ6SE',
-    tortas:'https://youtu.be/rIoJXtbl9Es',
-    postre:'https://youtu.be/pNSWtvuawQI',
-    destacado1: 'https://youtu.be/PhpmSb58zIc',
-    malteadas:'https://youtu.be/04UmtEeDbVw',
-    sedes: 'https://youtu.be/HE4T2fGujYo',
-    te_chai: 'https://youtu.be/eEQbnoevhF0'
-
+    a_cocteles,
+    a_tortas,
+    a_postre,
+    a_destacado1,
+    a_malteadas,
+    sedes,
+    taller,
+    te_chai
 };
 
 export const MORNING_ALAMEDA={
-    desayunos:'https://youtu.be/VdlVi6IASOg',
-    destacado2: 'https://youtu.be/1HJNM8RCQSM',
-    tortas:'https://youtu.be/rIoJXtbl9Es',
-    sedes: 'https://youtu.be/HE4T2fGujYo',
-    te_chai: 'https://youtu.be/eEQbnoevhF0'    
+    desayunos,
+    a_destacado2,
+    a_tortas,
+    sedes,
+    taller,
+    te_chai    
 };
 
 export const TARDE_ALAMEDA={
-    tortas:'https://youtu.be/rIoJXtbl9Es',
-    postre:'https://youtu.be/pNSWtvuawQI',
-    cocteles:'https://youtu.be/R-KPSDuQ6SE',
-    destacado1: 'https://youtu.be/PhpmSb58zIc',
-    malteadas:'https://youtu.be/04UmtEeDbVw',
-    sedes: 'https://youtu.be/HE4T2fGujYo',
-    te_chai: 'https://youtu.be/eEQbnoevhF0'
+    a_tortas,
+    a_postre,
+    a_cocteles,
+    a_destacado1,
+    a_malteadas,
+    sedes,
+    taller,
+    te_chai
 };
 
-
-export const SAN_ANTONIO = {
-
-    malteadas:'https://youtu.be/0Qr9esjf1Gw',
-    desayunos:'https://youtu.be/vol1T72N_3k',
-    cocteles:'https://youtu.be/R-KPSDuQ6SE',
-    focaccia_sandwiches:'https://youtu.be/JFdiQ4xVp7Y',
-    cafe_licor: 'https://youtu.be/T5NYEPxDXC4',
-    postres:'https://youtu.be/fFNotkvMB5Q',
-    sedes: 'https://youtu.be/HE4T2fGujYo',
-    te_chai: 'https://youtu.be/eEQbnoevhF0'
-
-};
 
 export const MORNING_SAN_ANTONIO ={
-    desayunos:'https://youtu.be/vol1T72N_3k',
-    focaccia_sandwiches:'https://youtu.be/JFdiQ4xVp7Y',
-    sedes: 'https://youtu.be/HE4T2fGujYo',
-    te_chai: 'https://youtu.be/eEQbnoevhF0'
+    desayunos,
+    focaccia_sandwiches,
+    sedes,
+    taller,
+    te_chai
 }
 
 export const TARDE_SAN_ANTONIO ={
-    malteadas:'https://youtu.be/0Qr9esjf1Gw',
-    postres:'https://youtu.be/fFNotkvMB5Q',
-    sedes: 'https://youtu.be/HE4T2fGujYo',
-    te_chai: 'https://youtu.be/eEQbnoevhF0'
+    malteadas,
+    postres,
+    sedes,
+    taller,
+    te_chai
 }
 
 export const NOCHE_SAN_ANTONIO ={
-    cafe_licor: 'https://youtu.be/T5NYEPxDXC4',
-    cocteles:'https://youtu.be/R-KPSDuQ6SE',
-    sedes: 'https://youtu.be/HE4T2fGujYo',
-    te_chai: 'https://youtu.be/eEQbnoevhF0'
+    cafe_licor,
+    cocteles,
+    sedes,
+    taller,
+    te_chai
 }
